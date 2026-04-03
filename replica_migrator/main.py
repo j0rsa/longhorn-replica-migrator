3edcaaff87f9cefce2af13e386bd6d10981ee92d
red <red.avtovo@gmail.com>
2026-04-03 20:25:55 +0200

    Fix status line hidden after padding change: use top-only padding

--- replica_migrator/main.py
+++ replica_migrator/main.py
@@ -444,7 +444,7 @@ class MigrationScreen(Screen[None]):
         layout: vertical;
     }
     #status_line {
-        padding: 1 2;
+        padding: 1 2 0 2;
         height: 3;
         background: $surface;
         border-bottom: solid $primary;
